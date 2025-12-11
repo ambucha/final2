f1a86bd86793f408d2e57a8ebb9f5d22e21c376a
ambucha <ambuchatskiy@gmail.com>
2025-12-11 11:55:17 -0800

    commit 4

--- final2.cpp
+++ final2.cpp
@@ -36,12 +36,32 @@ class Coffee{
         public:
             Coffee(){ head = nullptr; tail = nullptr;}
 
-            // we need to check if there is anyone in line
-            bool isEmpty(){
-                return head == nullptr; // this checks if the string is empty and returns true or false
-            }
+            // i was reading over my DLL and there actually isnt a need for an isempty function since i check before i do anything to it anyways
 
             // now we need to add someone to the back of the line
-            
+            void push_back(string n, string d){
+                Node* newNode = new Node(n, d);
+                if(!tail){// if no tail then empty
+                    head = tail = newNode;
+                } 
+                else {
+                    tail->next = newNode;
+                    newNode -> prev = tail;
+                    tail = newNode;
+                }
+            }
+
+            // now we add something for serving people, i can make this a bool so that if the string is empty then noone is served
+            // using variables here will allow us to be able to actually print who got served, initially i was gonna leave it out but then realized it would be hard to say who got served each round that way
+            bool pop_front(string &n, string &d){
+                if(!head){
+                    return false; // here is where using a bool function si good so later in the main function we can use it as a conditional for if someone is served
+                }
+
+                Node* temp = head;
+                n = temp->name;
+                d = temp->drink;
+
+                head = temp->next;
 
 };
