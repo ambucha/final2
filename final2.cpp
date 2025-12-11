11271e7a14f4f554cd6144a2fb43a8ef7e716f5a
ambucha <ambuchatskiy@gmail.com>
2025-12-11 11:31:51 -0800

    commit 2

---
+++ final2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Milestone 1
+
+// seems this time the isntructions did not say to not write comments so i will just write them
+// ok so lets start off by making the struct that has the customer name and drink order
+struct Customer {
+    string name;
+    string drink;
+};
+
+// lemme try to find an old project with a linked list so i can modify that code
